fe2fc0b0364fa33c1ff390fd3d47bde8109fe16b
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-10-20 19:59:38 -0300

    Add a basic Users controller

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,14 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add usage of controllers
+builder.Services.AddControllers();
+
+// Add Swagger UI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Uses Swagger UI during development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+// Maps all controller classes
+app.MapControllers();
 
 app.Run();
